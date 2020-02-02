8651ab9753fbcbaff8b9dfd3fe1f7ffc5eb9a709
gnanasuriyan-gnanaoli <gnanasuriyan.gnanaoli@freshworks.com>
2020-02-02 18:53:54 +0530

    It Follows

---
+++ it-follows/Assets/scripts/Chaser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaser : MonoBehaviour
+{
+
+    public float speed;
+    public Transform playerTransform;
+    public GameManager gameManager;
+
+    void Update()
+    {
+        Vector3 displacement = playerTransform.position - transform.position;
+        Vector3 direction = displacement.normalized;
+        Vector3 velocity = speed * direction;
+        Vector3 moveAmount = velocity * Time.deltaTime;
+        if(displacement.magnitude < 20f)
+        {
+            if (displacement.magnitude > 1.5f)
+            {
+                transform.Translate(moveAmount);
+            }
+            else
+            {
+                gameManager.GameOver();
+            }
+        }
+        
+    }
+}
---
+++ it-follows/Assets/scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = input.normalized;
+        Vector3 velocity = speed * direction;
+        Vector3 moveAmount = velocity * Time.deltaTime;
+
+        transform.Translate(moveAmount);
+
+
+    }
+}
